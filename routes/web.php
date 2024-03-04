4879559437c3fb513143e8ae72d91e0f5f455ef2
B00144009 Rabia Saeed <b00144009@mytudublin.ie>
2024-03-04 16:35:05 +0000

    got the website up now

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('products/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
+
+//Route::resource('products', App\Http\Controllers\productController::class);
+
+
+Route::resource('orderdetails', App\Http\Controllers\orderdetailController::class);
+
+
+Route::resource('scorders', App\Http\Controllers\scorderController::class);
+
+Route::get('products/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
